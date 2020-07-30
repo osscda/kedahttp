0af6eddd234e6073953d3b12f215c518dba04470
Aaron Schlesinger <aaron@ecomaz.net>
2020-07-30 16:47:18 -0700

    adding scale-up and scale down logic

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -39,29 +39,13 @@ func main() {
 		log.Fatalf("Couldn't subscribe to 'scaled' channel")
 	}
 
-	go func() {
-		for {
-			msg := <-scaledCh
-			err := db.Update(func(tx *bolt.Tx) error {
-				bucket, err := tx.CreateBucketIfNotExists([]byte("containers"))
-				if err != nil {
-					return fmt.Errorf("Couldn't create 'containers' bucket (%s)", err)
-				}
-				containerURL := msg.Data
-				bucket.Put(containerURL, nil)
-				return nil
-			})
-			if err != nil {
-				break
-			}
-		}
-	}()
+	go listener(nc)
 
 	// TODO: listen for scale-down events from the controller
 
 	e := echo.New()
-	e.GET("/pong", newPongHandler())
-	e.Any("/*", newHomeHandler(
+	e.GET("/pong", pongHandler)
+	e.Any("/*", newForwardingHandler(
 		func() { nc.Publish("reqCounter", nil) },
 		scaledCh,
 		db,
@@ -80,61 +64,11 @@ func main() {
 	e.Start(fmt.Sprintf(":%s", port))
 }
 
-func newPongHandler() echo.HandlerFunc {
-	return echo.HandlerFunc(func(c echo.Context) error {
-		reqBytes, err := httputil.DumpRequest(c.Request(), true)
-		if err != nil {
-			return err
-		}
-		log.Printf("/pong incoming request: %v", string(reqBytes))
-		return c.String(http.StatusOK, string(reqBytes))
-	})
-}
-
-func newHomeHandler(incrementReq func(), scaledCh <-chan *nats.Msg, db *bolt.DB) echo.HandlerFunc {
-	return echo.HandlerFunc(func(c echo.Context) error {
-		httpReq := c.Request()
-		// check once to see if there's a container available
-		containerURL := getContainerEndpoint(db)
-		if containerURL == "" {
-			log.Printf(
-				"Waiting for backend container for %s%s",
-				httpReq.URL.Host,
-				httpReq.URL.String())
-			msg := <-scaledCh
-			// don't wait for the URL to be in the DB.
-			// just get it right away and let the
-			// watcher goroutine put it in the DB
-			// asynchronously
-			containerURL = string(msg.Data)
-		}
-		// forward the request
-		http.DefaultClient.Do(httpReq)
-		incrementReq()
-		return nil
-	})
-}
-
-func getContainerEndpoint(db *bolt.DB) string {
-	var containerURLs []string
-	err := db.View(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte("containers"))
-		if err != nil {
-			return fmt.Errorf("Couldn't create bucket (%s)", err)
-		}
-		bucket.ForEach(func(k, v []byte) error {
-			containerURLs = append(containerURLs, string(k))
-			return nil
-		})
-		return nil
-	})
-
+func pongHandler(c echo.Context) error {
+	reqBytes, err := httputil.DumpRequest(c.Request(), true)
 	if err != nil {
-		return ""
+		return err
 	}
-
-	// no real load balancing for now. just get a random container
-	// endpoint
-	idx := rand.Intn(len(containerURLs))
-	return containerURLs[idx]
+	log.Printf("/pong incoming request: %v", string(reqBytes))
+	return c.String(http.StatusOK, string(reqBytes))
 }
