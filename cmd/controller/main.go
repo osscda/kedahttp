940abab513560c7a23a6f2ed9f2b80bd5748750e
Aaron Schlesinger <aaron@ecomaz.net>
2020-08-03 16:01:25 -0700

    add HPA link

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -6,13 +6,18 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
+// use approximately the same as the k8s HPA algorithm:
+// https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale/#algorithm-details
 func main() {
 	nc, err := nats.Connect(nats.DefaultURL)
 	if err != nil {
 		log.Fatalf("Error connecting to NATS (%s)", err)
 	}
+
 	nc.Subscribe("reqcounter", func(m *nats.Msg) {
 		log.Printf("reqcounter %v", *m)
+		// send dummy scale up event so that the proxy can
+		// forward to something for now.
 		nc.Publish("scaled.up", []byte("https://gifm.dev"))
 		log.Printf("sent scale.up")
 	})
