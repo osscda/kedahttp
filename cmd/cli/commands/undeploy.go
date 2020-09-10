183d6ab4fdc714fff7f7988531fe6453c24e2e5c
Aaron Schlesinger <aaron@ecomaz.net>
2020-09-10 15:28:23 -0700

    CLI can deploy and undeploy

---
+++ cmd/cli/commands/undeploy.go
@@ -0,0 +1,48 @@
+package cli
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/parnurzeal/gorequest"
+	"github.com/spf13/cobra"
+)
+
+func newUndeployCmd() *cobra.Command {
+	var serverURL string
+	var undeployCmd = &cobra.Command{
+		Use:   "undeploy",
+		Short: "Remove a deployment",
+		Long:  `Remove a deployment by its name. This will delete all traces of the images running and immediately stop serving your app.`,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				log.Fatalf("You need to pass the deployment name")
+			}
+			cl := gorequest.New()
+
+			deployURL := fmt.Sprintf("https://%s/admin/deploy", serverURL)
+			_, _, errs := cl.Delete(deployURL).
+				Send(fmt.Sprintf("name=%s", args[0])).
+				End()
+			if len(errs) > 0 {
+				var result error
+				log.Printf("Error undeploying: %v", errs)
+				return multierror.Append(result, errs...)
+			}
+
+			log.Printf("Undeployed!")
+			return nil
+		},
+	}
+	undeployCmd.Flags().StringVarP(
+		&serverURL,
+		"server-url",
+		"s",
+		"wtfcncf.dev",
+		"The URL to the admin server (without the 'http' prefix",
+	)
+	return undeployCmd
+
+}
