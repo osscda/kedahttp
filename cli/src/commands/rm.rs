7ceb195e4b1457569a58ddc9fff0983ebead3fd0
Aaron Schlesinger <aaron@ecomaz.net>
2020-10-28 16:03:37 -0700

    test for rm
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- cli/src/commands/rm.rs
+++ cli/src/commands/rm.rs
@@ -6,3 +6,23 @@ pub async fn rm(ac: &mut impl AppClient, app_name: &str)
 -> Result<(), Error> {
     ac.rm_app(app_name).await
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::executor::block_on;
+    use crate::commands::client::TestAppClient;
+
+
+    #[test]
+    fn test_run() {
+        let mut cl = TestAppClient::new();
+        let res_fut = rm(&mut cl, "testapp");
+        let res = block_on(res_fut).unwrap();
+        
+        assert_eq!(res, ());
+        assert_eq!(cl.rm_counter, 1);
+        assert_eq!(cl.add_counter, 0);
+    }
+
+}
